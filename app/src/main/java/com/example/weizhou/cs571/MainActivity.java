ae0486737dbeea390aa9ea622bf9584a5a1b94b4
benben1026 <zhouwei1026@gmail.com>
2018-04-21 10:43:57 -0700

    Init

---
+++ app/src/main/java/com/example/weizhou/cs571/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.weizhou.cs571;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn =  findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    textView.setText(R.string.app_name);
+            }
+        });
+    }
+}
